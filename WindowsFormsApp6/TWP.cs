95f83eed55e51cb0670a228cc6b246f24442d2ca
qwerty <bill4678key@gmail.com>
2019-01-20 11:32:03 +0200

    all is working now)

---
+++ WindowsFormsApp6/TWP.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Threading.Tasks;
+using System.Net;
+using Titanium.Web.Proxy;
+using Titanium.Web.Proxy.EventArguments;
+using Titanium.Web.Proxy.Models;
+
+namespace WindowsFormsApp6
+{
+    public class TWP
+    {
+        ProxyServer proxyServer = new ProxyServer();
+        public void CreateProxySrvr()
+        {
+            proxyServer.CertificateManager.TrustRootCertificate(true);
+            proxyServer.BeforeRequest += OnRequest;
+            proxyServer.BeforeResponse += OnResponse;
+            proxyServer.ServerCertificateValidationCallback += OnCertificateValidation;
+            proxyServer.ClientCertificateSelectionCallback += OnCertificateSelection;
+            var explicitEndPoint = new ExplicitProxyEndPoint(IPAddress.Any, 8000, true);
+            proxyServer.AddEndPoint(explicitEndPoint);
+            proxyServer.Start();
+            var transparentEndPoint = new TransparentProxyEndPoint(IPAddress.Any, 8001, true)
+            {
+                GenericCertificateName = "google.com"
+            };
+
+            proxyServer.AddEndPoint(transparentEndPoint);
+            foreach (var endPoint in proxyServer.ProxyEndPoints)
+                Console.WriteLine("Listening on '{0}' endpoint at Ip {1} and port: {2} ",
+                    endPoint.GetType().Name, endPoint.IpAddress, endPoint.Port);
+            proxyServer.SetAsSystemHttpProxy(explicitEndPoint);
+            proxyServer.SetAsSystemHttpsProxy(explicitEndPoint);
+        }
+        private async Task OnBeforeTunnelConnectRequest(object sender, TunnelConnectSessionEventArgs e)
+        {
+            string hostname = e.HttpClient.Request.RequestUri.Host;
+
+            if (hostname.Contains("dropbox.com"))
+            {
+                e.DecryptSsl = false;
+            }
+        }
+        public async Task OnRequest(object sender, SessionEventArgs e)
+        {
+            Console.WriteLine(e.HttpClient.Request.Url);
+            if (e.HttpClient.Request.RequestUri.AbsoluteUri.Contains("google.com"))
+            {
+                e.Ok("<!DOCTYPE html>" +
+                      "<html><body><h1>" +
+                      "Website Blocked" +
+                      "</h1>" +
+                      "<p>Blocked by titanium web proxy.</p>" +
+                      "</body>" +
+                      "</html>");
+            }
+            //Redirect example
+            if (e.HttpClient.Request.RequestUri.AbsoluteUri.Contains("wikipedia.org"))
+            {
+                e.Redirect("https://www.paypal.com");
+            }
+        }
+        public async Task OnResponse(object sender, SessionEventArgs e)
+        {
+            var responseHeaders = e.HttpClient.Response.Headers;
+            if (e.HttpClient.Request.Method == "GET" || e.HttpClient.Request.Method == "POST")
+            {
+                if (e.HttpClient.Response.StatusCode == 200)
+                {
+                    if (e.HttpClient.Response.ContentType != null && e.HttpClient.Response.ContentType.Trim().ToLower().Contains("text/html"))
+                    {
+                        byte[] bodyBytes = await e.GetResponseBody();
+                        e.SetResponseBody(bodyBytes);
+
+                        string body = await e.GetResponseBodyAsString();
+                        e.SetResponseBodyString(body);
+                    }
+                }
+            }
+
+            if (e.UserData != null)
+            {
+                var request = (Titanium.Web.Proxy.Http.Request)e.UserData;
+            }
+        }
+        public Task OnCertificateValidation(object sender, CertificateValidationEventArgs e)
+        {
+            if (e.SslPolicyErrors == System.Net.Security.SslPolicyErrors.None)
+                e.IsValid = true;
+            return Task.FromResult(0);
+        }
+        public Task OnCertificateSelection(object sender, CertificateSelectionEventArgs e)
+        {
+            return Task.FromResult(0);
+        }
+        public void StopProxySrvr()
+        {
+            proxyServer.BeforeRequest -= OnRequest;
+            proxyServer.BeforeResponse -= OnResponse;
+            proxyServer.ServerCertificateValidationCallback -= OnCertificateValidation;
+            proxyServer.ClientCertificateSelectionCallback -= OnCertificateSelection;
+            proxyServer.Stop();
+        }
+    }
+}
